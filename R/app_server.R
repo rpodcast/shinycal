46a97cc0c9730ce17340c28de4f262f986305f4d
Eric Nantz <thercast@gmail.com>
2021-06-17 01:43:28 +0000

    use shinycal in app for cal import

--- R/app_server.R
+++ R/app_server.R
@@ -7,7 +7,9 @@
 app_server <- function( input, output, session ) {
   
   # import calendar from server
-  cal_df <- import_cal()
+  cal_slug <- "data-science-streams"
+  cal_base_url <- "https://nextcloud.r-podcastdev.link/remote.php/dav/calendars/shinycal"
+  cal_df <- import_cal(cal_base_url = cal_base_url, cal_slug = cal_slug)
   
   # Your application server logic 
   mod_cal_viewer_server("cal_viewer_ui_1", cal_df)

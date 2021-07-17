7e146c6f0894cbd2fb6ac0b78955123bd45131e4
Eric Nantz <thercast@gmail.com>
2021-07-17 01:30:10 +0000

    show a twitch video when user clicks an entry

--- R/mod_cal_viewer.R
+++ R/mod_cal_viewer.R
@@ -7,13 +7,20 @@
 #' @noRd 
 #'
 #' @importFrom shiny NS tagList 
-#' @importFrom toastui calendarOutput renderCalendar calendar cal_props
+#' @import toastui
+#' @import shinylogs
 mod_cal_viewer_ui <- function(id){
   ns <- NS(id)
   tagList(
     fluidRow(
-      col_12(
-        calendarOutput(ns("calui"))
+      col_8(
+        calendarOutput(ns("calui")),
+        uiOutput(ns("vid"))
+      ),
+      col_4(
+        h2("More Info Here"),
+        textInput(ns("blah"), "Enter something"),
+        verbatimTextOutput(ns("last_changed"))
       )
     )
   )
@@ -25,6 +32,55 @@ mod_cal_viewer_ui <- function(id){
 mod_cal_viewer_server <- function(id, cal_df){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
+
+    observeEvent(input$calui_click, {
+      message("hello")
+    })
+
+    output$vid <- renderUI({
+      js_snippet <- glue::glue("
+      <script type='text/javascript'>
+          var options = {
+            width: '800',
+            height: '600',
+            video: '1060733999',
+            autoplay: false
+            //parent: 'localhost'
+          };
+          var player = new Twitch.Player('<<<divid>>>', options);
+          player.setVolume(0.5);
+        </script>",
+        divid = ns("testvid"),
+        .open = "<<<",
+        .close = ">>>")
+      
+      tags$div(
+        id = ns("testvid"),
+        h2("vid was here")
+        #htmltools::HTML(js_snippet)
+      )
+    })
+
+    output$last_changed <- renderPrint({
+      
+      req(input$calui_schedules)
+
+      input_lst <- reactiveValuesToList(input)
+      print(input_lst)
+      # list(
+      #   click = input$calui_click,
+      #   dates = input$calui_dates,
+      #   schedules = input$calui_schedules
+      # )
+      #input$`.shinylogs_lastInput`
+      # Shiny.setInputValue(el.id + "_dates"
+      # _add
+      # _schedules
+      # _click
+      # _delete
+      # _update
+      # _dates
+    })
     
     output$calui <- renderCalendar({
       
@@ -33,6 +89,10 @@ mod_cal_viewer_server <- function(id, cal_df){
 
       # `YYYY-MM-DD XX:YY:ZZ`
       
+      my_id <- ns("fancy")
+      
+      # TODO: Change this to use the streamer_data frame
+
       cal_sub <- cal_df %>%
         mutate(start_clock = as_naive_time(start_clock), end_clock = as_naive_time(end_clock)) %>%
         mutate(start = as.character(start_clock), end = as.character(end_clock)) %>%
@@ -58,6 +118,16 @@ mod_cal_viewer_server <- function(id, cal_df){
             bgColor = "forestgreen",
             borderColor = "forestgreen"
           )
+        ) %>%
+        cal_events(
+          # schedule elements that we could grab
+          # - location
+          # - recurrenceRule
+          # - body
+          # - title
+          cal = .,
+          clickSchedule = JS(glue::glue('function(event) {console.log(event.schedule.id); Shiny.setInputValue("<<my_id>>", event.schedule.raw);}', .open = "<<", .close = ">>"))
+          #clickSchedule = JS("function(event) {alert(event.schedule.id);}")
         )
     })
  

445bded4a6457a2e51e7997288353215f95e3166
Eric Nantz <thercast@gmail.com>
2021-08-12 01:44:24 +0000

    revise streamer data yml files

--- data-raw/streamer_data.R
+++ data-raw/streamer_data.R
@@ -1,12 +1,18 @@
 ## code to prepare `streamer_data` dataset goes here
 devtools::load_all()
-yml_file <- "data-raw/streamers.yml"
-yml_data <- yaml::read_yaml(file = yml_file)
+# yml_file <- "data-raw/streamers.yml"
+# yml_data <- yaml::read_yaml(file = yml_file)
+
+yml_files <- fs::dir_ls("data-raw/yaml_files")
+
+yml_data <- purrr::map(yml_files, ~yaml::read_yaml(file = .x))
+
+#tibble::tibble(yml_data2) %>% tidyr::unnest_wider(1)
 
 # set up authentication via environment variables
 twitchr::twitch_auth()
 
-streamer_data <- tibble::tibble(yml_data$streamers) %>%
+streamer_data <- tibble::tibble(yml_data) %>%
     tidyr::unnest_wider(1) %>%
     dplyr::filter(platform == "twitch") %>%
     dplyr::mutate(user_data = purrr::map(user_id, ~get_twitch_id(user_name = .x))) %>%

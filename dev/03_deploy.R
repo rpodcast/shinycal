060bc9a73f43f9192eb0c3cb555263d6fda85a41
Eric Nantz <thercast@gmail.com>
2021-08-12 03:54:34 +0000

    fix deployment issues

--- dev/03_deploy.R
+++ dev/03_deploy.R
@@ -40,3 +40,17 @@ golem::add_dockerfile_shinyproxy()
 
 ## If you want to deploy to Heroku
 golem::add_dockerfile_heroku()
+
+## deploy app
+# if deploying in vs code container, must create /workspaces/rsconnect dir first
+# sudo mkdir /workspaces/rsconnect
+# sudo chown -R eric:eric /workspaces/rsconnect
+
+# one-time operation
+# rsconnect::setAccountInfo(
+#     name = "rpodcast",
+#     token = Sys.getenv("RSCONNECT_TOKEN"),
+#     secret = Sys.getenv("RSCONNECT_SECRET")
+# )
+
+rsconnect::deployApp(appName = "shinycal", appFileManifest = "dev/app_manifest.txt", launch.browser = FALSE)
